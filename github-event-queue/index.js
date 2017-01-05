3482f301c4afbe79c4051b999c91a9d796a297ed
R. Tyler Croy <tyler@monkeypox.org>
2017-01-05 14:49:44 -0800

    Update the format of the record to match that described in IEP-005

--- github-event-queue/index.js
+++ github-event-queue/index.js
@@ -1,22 +1,35 @@
 /*
- *
+ * Implementation of webhooks processing code for project events, as described
+ * in IEP-005 which can be found here:
+ *      <https://github.com/jenkins-infra/iep/pull/6>
  */
 
 module.exports = function (context, event) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.log(JSON.stringify(context.bindings));
 
     if (event) {
-        var gh_event = {
-            event: event,
+        var source = "unknown";
+        var event_type = "unknown";
+
+        if (event_type = context.req.headers['x-github-event']) {
+            source = "github";
+        }
+
+        var record = {
+            source   : source,
+            type     : event_type,
+            event    : event,
             received: new Date().toISOString()
         };
 
-        context.bindings.outputEventHubMessage = gh_event;
-        context.bindings.outputDocument = JSON.stringify(gh_event);
+        /* enqueue into the EventHub */
+        context.bindings.outputEventHubMessage = record;
+        /* append to the DocumentDB */
+        context.bindings.outputDocument = JSON.stringify(record);
+
         res = {
             // status: 200, /* Defaults to 200 */
-            body: "Thanks for the GitHub event"
+            body: "Thanks for the event!"
         };
     }
     context.done(null, res);
